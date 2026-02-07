ef524ad8b59d5d8fa339e8fa160644345ea3a8b9
Your Name <you@example.com>
2026-02-07 14:49:26 +0100

    Logout button in HomePage

--- src/test/java/logic/pages/HomePage.java
+++ src/test/java/logic/pages/HomePage.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.WebDriver;
 public class HomePage extends BasePage {
 
     private final By signupLoginBtn = By.linkText("Signup / Login");
+    private final By logoutBtn = By.linkText("Logout");
 
     public HomePage(WebDriver driver) {
         super(driver);
@@ -15,6 +16,10 @@ public class HomePage extends BasePage {
 
     public boolean isHomePageVisible() {
         return driver.getTitle().contains("Automation Exercise");
+    }
+
+    public void logout() {
+        click(logoutBtn);
     }
 
     @Step("User navigates to signup page")
