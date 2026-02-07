49496ea558db290ae6db8b37a7ab9de6d8988de3
Your Name <you@example.com>
2026-02-07 14:53:03 +0100

    Adopt old test cases to new helper methods

--- src/test/java/tests/RegisterAndDeleteUser.java
+++ src/test/java/tests/RegisterAndDeleteUser.java
@@ -4,14 +4,22 @@ import base.BaseTest;
 import logic.pages.*;
 import logic.utils.AdPopupUtils;
 import logic.utils.CookieUtils;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
-import static logic.utils.TestDataUtils.generateEmail;
+import java.time.Duration;
+import java.util.Optional;
+
 import static org.testng.Assert.assertTrue;
 
 @Listeners({io.qameta.allure.testng.AllureTestNg.class, utils.AllureListener.class})
 public class RegisterAndDeleteUser extends BaseTest {
+
+    private final String USERNAME = "TestUser";
+    private final String EMAIL = "TestUser123@mail.com";
+    private final String PASSWORD = "Password123";
 
     @Test
     public void shouldRegisterAndDeleteUser() {
@@ -25,22 +33,30 @@ public class RegisterAndDeleteUser extends BaseTest {
         LoginSignupPage login = home.goToSignupLogin();
         assertTrue(login.isNewUserSignupVisible());
 
-        AccountInformationPage account =
-                login.signup("TestUser", generateEmail());
+        Optional<AccountInformationPage> accountOpt =
+                login.signup(USERNAME, EMAIL);
 
-        AccountCreatedPage created =
-                account.fillAccountDetails("Password123")
-                        .fillAddressAndCreateAccount(
-                                "Jan", "Kowalsky", "Zielony Las 5", "Ontario", "Toronto", "12345", "123456789");
+        if (accountOpt.isEmpty()) {
+            login.login(EMAIL, PASSWORD);
+        } else {
+            AccountCreatedPage created =
+                    accountOpt.get().fillAccountDetails(PASSWORD)
+                            .fillAddressAndCreateAccount(
+                                    "Jan", "Kowalsky",
+                                    "Zielony Las 5", "Ontario",
+                                    "Toronto", "12345",
+                                    "123456789"
+                            );
 
-        assertTrue(created.isAccountCreatedVisible());
-
-        home = created.continueToHome();
-
+            assertTrue(created.isAccountCreatedVisible());
+            home = created.continueToHome();
+        }
         AdPopupUtils.killAdIfPresent(driver);
 
+        assertTrue(home.isHomePageVisible());
+
         HeaderComponent header = new HeaderComponent(driver);
-        assertTrue(header.isUserLoggedIn());
+        assertTrue(header.isUserLoggedIn(USERNAME));
 
         header.deleteAccount();
     }
